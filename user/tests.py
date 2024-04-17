8a7a6ea51c162b370481dde77450057d7166b231
Ronald Barbosa <ronaldverissimo@hotmail.com>
2024-04-16 23:29:25 -0300

    Implementado teste unitário para User model; atualizado arquivo yml.

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from user.api.serializers import CustomTokenObtainPairSerializer
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='spw_2024', email='test@email.com')
+
+    def test_login(self):
+        self.assertTrue(self.user.is_authenticated)
+
+        serializer = CustomTokenObtainPairSerializer()
+        token = serializer.get_token(self.user)
+        self.assertEqual(token['username'], self.user.username)
+        self.assertEqual(token['is_staff'], self.user.is_staff)
